0d91a8de3869ffeaef30ef0809cebef1a342c948
TDCIAN <tdcian71@gmail.com>
2021-08-20 09:35:40 +0900

    Initial Commit

---
+++ SwiftUIResizingTextView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIResizingTextView
+//
+//  Created by 김정민 on 2021/08/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIResizingTextView/SwiftUIResizingTextViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIResizingTextViewApp.swift
+//  SwiftUIResizingTextView
+//
+//  Created by 김정민 on 2021/08/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIResizingTextViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
